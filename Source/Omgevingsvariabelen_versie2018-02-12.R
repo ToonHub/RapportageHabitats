b6fbc8dd1c1b78bcf802a60fbd29caa5c682fd6d
ToonHub <toon.westra@inbo.be>
2018-05-17 11:12:09 +0200

    correct path

--- Source/Omgevingsvariabelen_versie2018-02-12.R
+++ Source/Omgevingsvariabelen_versie2018-02-12.R
@@ -57,7 +57,7 @@ dbVBIAnalyse <- "../Data/MeetgegevensVBI2/Versie3/VBI_AnalyseDatabank_v2016-08-3
 #---------------------
 
 dbVBIExterneData<-"../Data/ExterneData/VBIExterneData.accdb"
-schaalInfo  <- "../LSVIData/ExterneData/schalen.csv"
+schaalInfo  <- "../LSVIData/schalen.csv"
 
 #---------------------
 #../Databank strata
